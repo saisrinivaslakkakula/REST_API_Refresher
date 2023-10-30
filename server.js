334d7e8e71eb02a14c75e6185131cebb26f1d86a
saisrinivaslakkakula <saisrinivaslakkakula@Sais-MacBook-Pro.local>
2023-10-30 01:46:58 -0700

    Created simple server using express js and dotenv

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
-const http = require('http')
+const express = require('express')
+const dotenv = require('dotenv')
 
-const todos = [
-    {id:1, text:"To do 1"},
-    {id:2, text:"To do 2"},
-    {id:3, text:"To do 3"}
-]
-const server = http.createServer((req,res) => {
-    /*
-    This is a simple response when home route ('/') is requested.
-    we send a json response back to the client with the list of todos.
-    we have to set content-type of response header to application/json
-    set res.write(json data)
-    since the response object is not in standard json format. i.e. keys with "" 
-    and string columns with "" etc,
-    What we need to do is, we need to use JSON.Stringify() to wrap the 
-    response JSON into it and send it back to the client.
+// Load env vars
 
-    Note: this example is just for understanding only. Most of the jargon here
-    will be removed once we install express framework.
-    */
-    res.setHeader("Content-Type",'application/json')
-    const resp = {
-        success: true,
-        data : todos
-    }
-    res.write(JSON.stringify(resp))
-    res.end()
-})
+dotenv.config({path: './config/config.env'})
 
-const PORT = 5001
+const app = express()
 
-server.listen(PORT, ()=>{console.log(`Server started on PORT ${PORT}`)})
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, console.log(` Server is running in ${process.env.NODE_ENV} mode on port  ${PORT}`))
